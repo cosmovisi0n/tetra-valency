21f6f1a9b4f4768badd0bb2e15f7f277b6feee5d
Thedeaced <turkburhan01@gmail.com>
2026-03-10 20:11:59 +0300

    Create savemanager.java

---
+++ core/src/main/java/com/td/game/systems/SaveManager.java
@@ -0,0 +1,10 @@
+package com.td.game.systems;
+
+public class SaveManager {
+
+    private static final String SAVE_DIR = "Documents/TetraValency/";
+    
+    public static final String SAVE_FILE_ELEMENTAL_CASTLE = SAVE_DIR + "save_elemental_castle.json";
+    public static final String SAVE_FILE_DESERT_OASIS = SAVE_DIR + "save_desert_oasis.json";
+
+}
